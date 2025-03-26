d145ffaefb389d189b2d056c3e83b2943413e545
Asish K Antony <asishantony@gmail.com>
2025-03-26 23:35:03 +0530

    fixed rem values

--- wp-content/themes/savant-assignment/build/banner/index.asset.php
+++ wp-content/themes/savant-assignment/build/banner/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '81d212eacd01b5d62b92');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '34bf3471c866f4fd601e');
