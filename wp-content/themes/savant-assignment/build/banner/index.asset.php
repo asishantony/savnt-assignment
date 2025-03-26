150124ec25d1cfd31e669f5f9a81adafcce63dba
Asish K Antony <asishantony@gmail.com>
2025-03-26 23:31:08 +0530

    font changes

--- wp-content/themes/savant-assignment/build/banner/index.asset.php
+++ wp-content/themes/savant-assignment/build/banner/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3805c1f2dd6ce702e7ad');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '81d212eacd01b5d62b92');
